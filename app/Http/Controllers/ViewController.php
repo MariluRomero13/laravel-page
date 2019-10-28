13119aa0b558a6da31e61c56ce7fcb1727d54c6a
MariluRomero13 <romeromarilu13@gmail.com>
2019-10-27 22:31:43 -0600

    [FIX] problems with styles fixed

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -6,13 +6,18 @@ use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
+    public function login()
+    {
+        return view('page.login');
+    }
+
     public function main()
     {
         return view('page.main');
     }
 
-    public function login()
+    public function whatsNew()
     {
-        return view('page.login');
+        return view('page.whats-new');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/layout', function () {
-    return view('layouts.layout');
-});
-
-Route::get('/index', function () {
-    return view('page.index');
-});
-
-Route::get('/login', function () {
-    return view('page.login');
-});
-
-Route::get('/whats-new', function(){
-    return view('page.whats-new');
-});
+Route::get('/', 'ViewController@main');
+Route::get('/login', 'ViewController@login');
+Route::get('/whats-new', 'ViewController@whatsNew');
