5e731861921acd4c411c8863340dabee12426c6e
MariluRomero13 <romeromarilu13@gmail.com>
2019-10-27 21:27:45 -0600

    [ADD] index view

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,8 @@
 (function () {
-    var burger = document.querySelector('.burger');
-    var nav = document.querySelector('#' + burger.dataset.target);
+    var burger = document.querySelector('.burger')
+    var nav = document.querySelector('#' + burger.dataset.target)
     burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        nav.classList.toggle('is-active');
-    });
-})();
-
-document.querySelectorAll('.modal-button').forEach(function (el) {
-    el.addEventListener('click', function () {
-        var target = document.querySelector(el.getAttribute('data-target'))
-
-        target.classList.add('is-active')
-
-        target.querySelector('.modal-close').addEventListener('click', function () {
-            target.classList.remove('is-active')
-        })
+        burger.classList.toggle('is-active')
+        nav.classList.toggle('is-active')
     })
-})
+})()
