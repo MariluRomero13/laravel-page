10c8ddc4987ede15785a25121e05a71971926ead
MariluRomero13 <romeromarilu13@gmail.com>
2019-11-05 15:31:53 -0600

    [ADD] pages linked

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ Route::get('/login', 'ViewController@login');
 Route::get('/whats-new', 'ViewController@whatsNew');
 Route::get('/formacion-asistencia', 'ViewController@formacionAsistencia');
 Route::get('/introduccion-photoshop', 'ViewController@index');
-Route::get('/tutorial', 'ViewController@tutorial');
+Route::get('/tutorial-detalles', 'ViewController@tutorialDetalle');
 Route::get('/prueba-gratuita', 'ViewController@pruebaGratuita');
-Route::get('/download','ViewController@download');
+Route::get('/download', 'ViewController@download');
